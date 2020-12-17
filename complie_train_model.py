a4f1e2f91943e69b0e7afb030d3e04544df5006d
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-12-17 11:26:08 +0100

    change file location, add docstrings

--- complie_train_model.py
+++ complie_train_model.py
@@ -1,40 +1,40 @@
+"""
+Compiles and trains the model.
+
+"""
+
 import pickle
 import numpy as np
 from tensorflow import keras
 
-from generate_sequence import create_trianing_data
+from create_train_sequence import create_trianing_data
 from tokenize_words import max_seq_lenght, load_clean_descriptions, load_photos
 from create_model import create_model_img
 
 # Load needed data
-with open('tokenizer.pkl', 'rb') as f:
+with open('./model_files/tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
 
-with open('features2.pkl', 'rb') as f:
+with open('./model_files/features2.pkl', 'rb') as f:
     img_features = pickle.load(f)
 
-with open('embedding_vectors.pkl', 'rb') as f:
+with open('./model_files/embedding_vectors.pkl', 'rb') as f:
     embedding_vectors = pickle.load(f)
 
 # get training data
 
 filename = './Flickr8k_text/Flickr_8k.trainImages.txt'
 train = load_photos(filename)
-train_descriptions = load_clean_descriptions('./description.json', train)
+train_descriptions = load_clean_descriptions(
+    './model_files//description.json', train)
 
+# get max length of caption and vocab size
 max_length_caption = max_seq_lenght(train_descriptions)
 vocab_size = len(tokenizer.word_index) + 1
 
+# instantiate the input-output generator function
 train_data = create_trianing_data(
     train_descriptions, img_features, tokenizer, max_length_caption, vocab_size, 32)
-
-
-# # validation data set
-# filename_v = 'Flickr8k_text/Flickr_8k.devImages.txt'
-# validation = load_photos(filename_v)
-# valid_descriptions = load_clean_descriptions('./description.json', validation)
-# valid_data = create_trianing_data(
-#     valid_descriptions, img_features, tokenizer, max_length_caption, vocab_size, 32)
 
 
 # initialize model
@@ -54,4 +54,5 @@ history = model_cap.fit_generator(generator=train_data, epochs=40,
                                   steps_per_epoch=steps_per_epochs,
                                   callbacks=[callback])
 
-model_cap.save("Image_caption_v1.h5")
+# Save model
+model_cap.save("./model_files/Image_caption_v1.h5")
